3c32d9bb1af1695c55013157279752d891a80d46
dibaal <dibaal4real@yahoo.com>
2024-07-21 12:39:07 +0100

    pres

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "basic-terraform-state-bucket"  # Replace with your desired bucket name
+  lifecycle {
+    prevent_destroy = false  # Set to true to prevent accidental deletion of the bucket
+  }
+
+  tags = {
+    environment = "production"
+    provisioner = "terraform"
+    managed_by  = "terraform"
+  }
+}
