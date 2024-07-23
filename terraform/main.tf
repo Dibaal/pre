ecac6413697cbb2e8556b77d652af5ba1b4beebb
dibaal <dibaal4real@yahoo.com>
2024-07-23 21:10:09 +0100

    agile pres

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "basic-terraform-state-bucket"  # Replace with your desired bucket name
+  bucket = "basic-terraform-state-bucket1"  # Replace with your desired bucket name
   lifecycle {
     prevent_destroy = false  # Set to true to prevent accidental deletion of the bucket
   }
